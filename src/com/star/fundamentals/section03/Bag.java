84509cdd25354f9ca8920fc458a0d4ac0e01cf50
crystalDf <chendong333@gmail.com>
2017-05-26 22:56:06 +0800

    Chapter 01

---
+++ src/com/star/fundamentals/section03/Bag.java
@@ -0,0 +1,26 @@
+package com.star.fundamentals.section03;
+
+import java.util.Iterator;
+
+public class Bag<Item> implements Iterable<Item> {
+
+    public Bag() {
+    }
+
+    public void add(Item item) {
+
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return null;
+    }
+}
